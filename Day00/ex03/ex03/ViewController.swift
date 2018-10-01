1b03cb38c8311d63d732b278a8696f984a730722
kmodipa <kmodipa@student.42.fr>
2018-10-01 16:40:30 +0200

    added the complete functionality of the numbers and operations

--- Day00/ex03/ex03/ViewController.swift
+++ Day00/ex03/ex03/ViewController.swift
@@ -11,19 +11,32 @@ import UIKit
 class ViewController: UIViewController {
     
     var numberOnScreen:Double = 0;
+    var previousNumber:Double = 0;
+    var mathOperation = false;
+    var operation = 0;
+    var result:Double = 0;
     @IBOutlet weak var display: UILabel!
     
     /* The number keypad in one function */
     @IBAction func numbers(_ sender: UIButton)
     {
-        if display.text == "0"
+        if display.text == "0" || result > 0
         {
             display.text = ""
             display.text = display.text! + String(sender.tag-1)
         }
         else
         {
-            display.text = display.text! + String(sender.tag-1)
+            if mathOperation == true
+            {
+                display.text = String(sender.tag - 1)
+                numberOnScreen = Double(display.text!)!
+                mathOperation = false;
+            }
+            else
+            {
+                display.text = display.text! + String(sender.tag-1)
+            }
         }
         numberOnScreen = Double(display.text!)!
     }
@@ -31,7 +44,61 @@ class ViewController: UIViewController {
     /* The function keypad in one function */
     @IBAction func functionButtons(_ sender: UIButton)
     {
-        
+        if sender.tag != 11 && sender.tag != 16
+        {
+            previousNumber = Double(display.text!)!
+            
+            if sender.tag == 12 /* Addition */
+            {
+                display.text = "+"
+            }
+            else if sender.tag == 13 /* Subtraction */
+            {
+                display.text = "-"
+            }
+            else if sender.tag == 14 /* Multiplication */
+            {
+                display.text = "x"
+            }
+            else if sender.tag == 15 /* Division */
+            {
+                display.text = "/"
+            }
+            
+            operation = sender.tag;
+            mathOperation = true;
+        }
+        else if sender.tag == 16 /* Equal Sign */
+        {
+            if operation == 12 /* Addition */
+            {
+                display.text = String(previousNumber + numberOnScreen)
+                result = Double(previousNumber + numberOnScreen)
+            }
+            else if operation == 13 /* Subtraction */
+            {
+                display.text = String(previousNumber - numberOnScreen)
+                result = Double(previousNumber - numberOnScreen)
+            }
+            else if operation == 14 /* Multiplication */
+            {
+                display.text = String(previousNumber * numberOnScreen)
+                result = Double(previousNumber * numberOnScreen)
+            }
+            else if operation == 15 /* Division */
+            {
+                display.text = String(previousNumber / numberOnScreen)
+                result = Double(previousNumber / numberOnScreen)
+            }
+        }
+        else if sender.tag == 11  /* Clearing the data on and off screen */
+        {
+            display.text = "0"
+            numberOnScreen = 0;
+            previousNumber = 0;
+            operation = 0;
+            mathOperation = false;
+        }
     }
     
     override func viewDidLoad() {
