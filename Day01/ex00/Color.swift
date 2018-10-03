9ed6550d8c5de72e9bf472771dd171c1696cf63c
kmodipa <kmodipa@student.42.fr>
2018-10-03 16:49:26 +0200

    fixed wrong namming

--- Day01/ex00/Color.swift
+++ Day01/ex00/Color.swift
@@ -2,8 +2,8 @@
 enum Color:String {
     case Clubs = "13 Balck Clubs"
     case Spades = "13 Black Spades"
-    case Heards = "13 Red Heards"
+    case Hearts = "13 Red Heards"
     case Diamonds = "13 Red Diamods"
 
-    static let allColors = [Clubs, Spades, Heards, Diamods]
+    static let allColors = [Clubs, Spades, Hearts, Diamonds]
 }
