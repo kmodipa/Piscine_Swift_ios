27a7b23ea66bd1e6f945bcd10cd2f97140b7b90d
kmodipa <kmodipa@student.42.fr>
2018-10-03 17:45:33 +0200

    main with tests

---
+++ Day01/ex02/main.swift
@@ -0,0 +1,19 @@
+/* Spades */
+print("[All Spades]")
+print(Deck.allSpades)
+
+/* Hearts */
+print("\n[All Hearts]")
+print(Deck.allHearts)
+
+/* Clubs */
+print("\n[All Clubs]")
+print(Deck.allClubs)
+
+/* Diamonds */
+print("\n[All Diamonds]")
+print(Deck.allDiamonds)
+
+/* Cards */
+print("\n[All Cards]")
+print(Deck.allCards)
