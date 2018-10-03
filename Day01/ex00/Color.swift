97b374d74bc2faf20f162d0ad1f6628944f672d1
kmodipa <kmodipa@student.42.fr>
2018-10-03 12:57:17 +0200

    created Color and Value enums

---
+++ Day01/ex00/Color.swift
@@ -0,0 +1,9 @@
+
+enum Color:String {
+    case Red = "Red"
+    case Green = "Green"
+    case Blue = "Blue"
+    case Black = "Black"
+
+    static let allColors = [Red, Green, Blue, Black]
+}
---
+++ Day01/ex00/main.swift
@@ -0,0 +1,6 @@
+#!/usr/bin/env swift
+
+import Foundation
+
+print(Color.allColors)
+print(Value.allValues)
