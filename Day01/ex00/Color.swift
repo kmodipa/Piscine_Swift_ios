19397aa08cc53ca7640718980208b3bdba468eea
kmodipa <kmodipa@student.42.fr>
2018-10-03 16:09:21 +0200

    updated the color enum with the correct card colors

--- Day01/ex00/Color.swift
+++ Day01/ex00/Color.swift
@@ -1,9 +1,9 @@
 
 enum Color:String {
-    case Red = "Red"
-    case Green = "Green"
-    case Blue = "Blue"
-    case Black = "Black"
+    case Clubs = "13 Balck Clubs"
+    case Spades = "13 Black Spades"
+    case Heards = "13 Red Heards"
+    case Diamonds = "13 Red Diamods"
 
-    static let allColors = [Red, Green, Blue, Black]
+    static let allColors = [Clubs, Spades, Heards, Diamods]
 }
