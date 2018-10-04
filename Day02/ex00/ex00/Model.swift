811604d8997bf60305ce095b480f1db0530c7721
kmodipa <kmodipa@student.42.fr>
2018-10-04 17:30:07 +0200

    added a second Segue and on the second Segue a label and Done Button

---
+++ Day02/ex00/ex00/Model.swift
@@ -0,0 +1,17 @@
+//
+//  Model.swift
+//  ex00
+//
+//  Created by Khomotjo MODIPA on 2018/10/04.
+//  Copyright © 2018 Khomotjo. All rights reserved.
+//
+
+import Foundation
+
+struct Data {
+    static let films : [(String, String)] = [
+        ("Rigby", "Car eccident"),
+        ("Skips", "Skipping"),
+        ("Benson", "over working")
+    ]
+}
--- Day02/ex00/ex00/ViewController.swift
+++ Day02/ex00/ex00/ViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    /* Returns the number of items from our data source */
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return Data.films.count
+    }
+    
+    /* Returns the data we use to populate our tableView */
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "deathCell")
+        cell?.textLabel?.text = Data.films[indexPath.row].0 /* Giving our textLabel some info */
+        cell?.detailTextLabel?.text = String(Data.films[indexPath.row].1)
+        return cell!
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
