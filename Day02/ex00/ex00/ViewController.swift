6284669510910c8915b42ceffc3c2559599e7b3b
kmodipa <kmodipa@student.42.fr>
2018-10-04 08:10:42 +0200

    ex00 initial

---
+++ Day02/ex00/ex00/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ex00
+//
+//  Created by Khomotjo MODIPA on 2018/10/04.
+//  Copyright © 2018 Khomotjo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
