8b567ad4439c36508fbaeb3a02424391f95d04fb
kmodipa <kmodipa@student.42.fr>
2018-10-05 16:55:06 +0200

    fixed the images sizes

--- Day03/Day03-APM/Day03-APM/ViewController.swift
+++ Day03/Day03-APM/Day03-APM/ViewController.swift
@@ -12,17 +12,35 @@ class ViewController: UIViewController, UICollectionViewDataSource, UICollection
 
     /* Data Source */
     let ImageArray:[String] = [
-        "http://allhdwallpapers.com/wp-content/uploads/2017/05/French-Bulldog-2.jpg",
-        "http://allhdwallpapers.com/wp-content/uploads/2017/05/French-Bulldog-6.jpg",
-        "http://allhdwallpapers.com/wp-content/uploads/2017/05/French-Bulldog-5.jpg",
-        "http://allhdwallpapers.com/wp-content/uploads/2017/05/French-Bulldog-4.jpg",
-        "http://allhdwallpapers.com/wp-content/uploads/2017/05/French-Bulldog-1.jpg",
-        "http://allhdwallpapers.com/wp-content/uploads/2017/05/French-Bulldog-3.jpg"
+        "http://s1.1zoom.me/b5457/460/Dogs_French_Bulldog_Colored_background_Glance_528929_3840x2160.jpg",
+        "http://s1.1zoom.me/b6550/598/Dogs_French_Bulldog_Glance_532467_3840x2160.jpg",
+        "http://s1.1zoom.me/b5957/459/Dogs_French_Bulldog_Puppy_Paws_529473_3840x2160.jpg",
+        "http://s1.1zoom.me/b5353/964/French_Bulldog_Dogs_Puppy_527703_3840x2160.jpg",
+        "http://s1.1zoom.me/b5457/460/Dogs_French_Bulldog_Colored_background_Glance_528929_3840x2160.jpg",
+        "http://s1.1zoom.me/b6550/598/Dogs_French_Bulldog_Glance_532467_3840x2160.jpg",
+        "http://s1.1zoom.me/b5957/459/Dogs_French_Bulldog_Puppy_Paws_529473_3840x2160.jpg",
+        "http://s1.1zoom.me/b5353/964/French_Bulldog_Dogs_Puppy_527703_3840x2160.jpg",
     ]
+    
+    @IBOutlet weak var myCollectionView: UICollectionView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        /* Setting the size */
+        let itemSize = UIScreen.main.bounds.width/2 - 2
+        
+        /* A new LayOut to override our current layout */
+        let layout = UICollectionViewFlowLayout()
+        layout.sectionInset = UIEdgeInsetsMake(0, 0, 0, 0) /*top, left, bottom, right*/
+        layout.itemSize = CGSize(width: itemSize  , height: itemSize)
+        
+        layout.minimumInteritemSpacing = 1
+        layout.minimumLineSpacing = 1
+        
+        /* Connecting the new layout */
+        myCollectionView.collectionViewLayout = layout
     }
 
     override func didReceiveMemoryWarning() {
