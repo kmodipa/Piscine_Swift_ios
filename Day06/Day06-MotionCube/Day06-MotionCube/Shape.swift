81db83dd61ace32e3a501bcba0ce4b287e5d4846
kmodipa <kmodipa@student.42.fr>
2018-10-10 17:05:40 +0200

    update

--- Day06/Day06-MotionCube/Day06-MotionCube/Shape.swift
+++ Day06/Day06-MotionCube/Day06-MotionCube/Shape.swift
@@ -10,7 +10,36 @@ import UIKit
 
 class Shape: UIView {
 
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.clear
+    }
     
+    required init(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    
+    override func draw(_ rect: CGRect) {
+        // Get the Graphics Context
+        if let context = UIGraphicsGetCurrentContext() {
+            
+            // Set the circle outerline-width
+            context.setLineWidth(10.0);
+            
+            // Set the circle outerline-colour
+            UIColor.red.set()
+            
+            // Create Circle
+            let center = CGPoint(x: frame.size.width/2, y: frame.size.height/2)
+            let radius = (frame.size.width - 10)/2
+            context.addArc(center: center, radius: radius, startAngle: 0.0, endAngle: .pi * 2.0, clockwise: true)
+            
+            // Draw
+            context.strokePath()
+        }
+    }
     /*
     // Only override draw() if you perform custom drawing.
     // An empty implementation adversely affects performance during animation.
--- Day06/Day06-MotionCube/Day06-MotionCube/ViewController.swift
+++ Day06/Day06-MotionCube/Day06-MotionCube/ViewController.swift
@@ -15,11 +15,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
         let Gesture = UIPanGestureRecognizer(target: self, action: #selector(panGesture(gesture:)))
         view.addGestureRecognizer(Gesture)
+        self.view.backgroundColor = UIColor.lightGray
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        for touch in touches {
+            // Set the Center of the Circle
+            // 1
+            let circleCenter = touch.location(in: view)
+            
+            // Set a random Circle Radius
+            // 2
+            let circleWidth = CGFloat(25 + (arc4random() % 50))
+            let circleHeight = circleWidth
+            
+            // Create a new CircleView
+            // 3
+            let circleView = Shape(frame: CGRect(x: circleCenter.x, y: circleCenter.y, width: circleWidth, height: circleHeight))
+            view.addSubview(circleView)
+        }
     }
 
     @IBAction func tapAction(_ sender: UITapGestureRecognizer) {
