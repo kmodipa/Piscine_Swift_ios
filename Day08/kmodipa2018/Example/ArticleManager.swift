dae5523689ff93dd1daa1945d6354a1438046002
kmodipa <kmodipa@student.42.fr>
2018-10-12 16:13:36 +0200

    added an ArticleManager class

---
+++ Day08/kmodipa2018/Example/ArticleManager.swift
@@ -0,0 +1,15 @@
+//
+//  ArticleManager.swift
+//  
+//
+//  Created by Khomotjo MODIPA on 2018/10/12.
+//
+
+import Foundation
+
+public class ArticleManager {
+    
+    func newArticle() {
+        print("I have no idea what to do")
+    }
+}
