b7e1d7ddbb17ebc24a776744b0e20c03e005d872
Khomotjo MODIPA <kmodipa@e5r11p2.wethinkcode.co.za>
2018-10-14 11:28:57 +0200

    added the drawLine and the navigate annotation

--- Rush01/Rush01-Plan42-GPS/Rush01-Plan42-GPS/ViewController.swift
+++ Rush01/Rush01-Plan42-GPS/Rush01-Plan42-GPS/ViewController.swift
@@ -17,6 +17,8 @@ class ViewController: UIViewController, MGLMapViewDelegate {
     /* Properties */
     var mapView: NavigationMapView!
     var navigateButton: UIButton!
+    var directionsRoute: Route?
+    let disneyland = CLLocationCoordinate2D(latitude: -26.1472491, longitude: 28.0348387)
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -52,9 +54,77 @@ class ViewController: UIViewController, MGLMapViewDelegate {
     }
     
     @objc func navigateButtonWasPressed(_ sender: UIButton) {
+        mapView.setUserTrackingMode(.none, animated: true)
         
+        /* Adding Annotation */
+        let annotation = MGLPointAnnotation()
+        annotation.coordinate = disneyland
+        annotation.title = "Start navigation"
+        mapView.addAnnotation(annotation)
+        
+        calculateRoute(from: (mapView.userLocation!.coordinate), to: disneyland) { (route, error) in
+            if error != nil
+            {
+                print("error setting route")
+            }
+        }
+    }
+    
+    /* delegate method that allows annotations to show callouts when tapped */
+    func mapView(_ mapView: MGLMapView, annotationCanShowCallout annotation: MGLAnnotation) -> Bool {
+        return true
+    }
+    
+    /* Present the navigation view controller when the callout is selected */
+    func mapView(_ mapView: MGLMapView, tapOnCalloutFor annotation: MGLAnnotation) {
+        let navigationViewController = NavigationViewController(for: directionsRoute!)
+        self.present(navigationViewController, animated: true, completion: nil)
     }
 
+    func calculateRoute(from originCor: CLLocationCoordinate2D, to destinationCor: CLLocationCoordinate2D, completion: @escaping (Route?, Error?) -> Void)
+    {
+        let origin = Waypoint(coordinate: originCor, coordinateAccuracy: -1, name: "Start")
+        let destination = Waypoint(coordinate: destinationCor, coordinateAccuracy: -1, name: "Destination")
+        
+        let options = NavigationRouteOptions(waypoints: [origin, destination], profileIdentifier: .automobileAvoidingTraffic)
+        
+        _ = Directions.shared.calculate(options, completionHandler: { (waypoints, routes, error) in
+            self.directionsRoute = routes?.first
+            /* Draw Route here */
+            self.drawRoute(route: self.directionsRoute!)
+            
+            let coordinatesBounds = MGLCoordinateBounds(sw: destinationCor, ne: originCor)
+            let insets = UIEdgeInsets(top: 50, left: 50, bottom: 50, right: 50)
+            let routeCam = self.mapView.cameraThatFitsCoordinateBounds(coordinatesBounds, edgePadding: insets)
+            self.mapView.setCamera(routeCam, animated: true)
+        })
+        
+    }
+    
+    func drawRoute(route: Route) {
+        guard route.coordinateCount > 0 else { return }
+        // Convert the route’s coordinates into a polyline
+        var routeCoordinates = route.coordinates!
+        let polyline = MGLPolylineFeature(coordinates: &routeCoordinates, count: route.coordinateCount)
+        
+        // If there's already a route line on the map, reset its shape to the new route
+        if let source = mapView.style?.source(withIdentifier: "route-source") as? MGLShapeSource {
+            source.shape = polyline
+        } else {
+            let source = MGLShapeSource(identifier: "route-source", features: [polyline], options: nil)
+            
+            // Customize the route line color and width
+            let lineStyle = MGLLineStyleLayer(identifier: "route-style", source: source)
+            lineStyle.lineColor = NSExpression(forConstantValue: #colorLiteral(red: 0.1897518039, green: 0.3010634184, blue: 0.7994888425, alpha: 1))
+            lineStyle.lineWidth = NSExpression(forConstantValue: 3)
+            
+            // Add the source and style layer of the route line to the map
+            mapView.style?.addSource(source)
+            mapView.style?.addLayer(lineStyle)
+        }
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
